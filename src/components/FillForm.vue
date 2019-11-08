713bb0ec7a0a52e5c0c718419993a8d5a7ecd70e
Shuqi Liao <373628977@qq.com>
2019-11-08 13:55:58 +0800

    inputs and outputs form finish in display

--- src/components/FillForm.vue
+++ src/components/FillForm.vue
@@ -1,17 +1,36 @@
 <template>
 	<div class="defi-form">
-		<ul class="defi-inputs">
-			<li
-				v-for="(input, idx) in inputs"
-				:key="idx"
-				class="input-desc">
-				<label>input{{input.index+1}}</label>
-				<input v-model="input.msg" id="input-1"/>
-				<button id="minus-btn" @click="deleteInput(idx)">减</button>
-			</li>
-		</ul>
-		<button id="add-btn" @click="addOneInput">ADD</button>
-		<input type="button" value="确认" @click="sendToEditor"/>
+		<div class="defi-main-content">
+			<div class="defi-input-section">
+			<ul class="defi-input-ul">
+				<li
+					v-for="(input, idx) in inputs"
+					:key="idx"
+					class="input-desc">
+					<label>input{{input.index+1}}</label>
+					<input v-model="input.msg" class="inputs-field"/>
+					<button class="minus-btn" @click="deleteInput(idx)"></button>
+				</li>
+			</ul>
+			<button class="add-btn" @click="addOneInput">add input</button>
+			</div>
+			<div class="defi-input-section">
+			<ul class="defi-input-ul">
+				<li
+					v-for="(output, idx) in outputs"
+					:key="idx"
+					class="input-desc">
+					<label>output{{output.index+1}}</label>
+					<input v-model="output.msg" class="inputs-field"/>
+					<button class="minus-btn" @click="deleteOutput(idx)"></button>
+				</li>
+			</ul>
+			<button class="add-btn" @click="addOneOutput">add output</button>
+			</div>
+		</div>
+
+
+		<input class="confirm-btn" type="button" value="确认" @click="sendToEditor"/>
 
 	</div>
 </template>
@@ -26,15 +45,20 @@
 
 		data() {
 			return {
-					childValue: String,
-					desc: "",
-					inputs: [
-						{
-							index: 0,
-							msg: "UserInput"
-						}
-
-					]
+				childValue: String,
+				desc: "",
+				inputs: [
+					{
+						index: 0,
+						msg: "UserInput"
+					}
+				],
+				outputs: [
+					{
+						index: 0,
+						msg: "ErrorOutput"
+					}
+				]
 			};
 		},
 
@@ -55,6 +79,20 @@
 				this.inputs.map( ele => {
 					ele.index = this.inputs.indexOf(ele)
 				})
+			},
+
+			addOneOutput() {
+					this.outputs.push({
+							index: this.outputs.length,
+							msg: ""
+					})
+			},
+
+			deleteOutput(index) {
+					this.outputs.splice(index, 1)
+					this.outputs.map( ele => {
+							ele.index = this.outputs.indexOf(ele)
+					})
 			}
 		}
 	};
@@ -62,30 +100,63 @@
 
 <style lang="less" scoped>
 	.defi-form {
-		height: 50%;
+		position: relative;
+		height: 400px;
 		width: 40%;
 		z-index: 1000;
 		background: #ffffff;
 		border-radius: 10px;
-		padding: 40px;
+		padding: 40px 40px 20px 40px;
 		display: flex;
 		flex-direction: column;
 	}
-	.input-desc {
+	.defi-main-content {
+		display: flex;
+		flex-direction: row;
 
 	}
-	#add-btn {
+	.input-desc {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+	}
+	.add-btn {
 		height: 25px;
 		width: 80px;
 		border-radius: 4px;
 		background: dodgerblue;
 		color: #ffffff;
+		float: left;
 	}
-	#minus-btn {
+	.minus-btn {
+		height: 20px;
+		width: 20px;
+		background-image: url("../assets/delete_btn.png");
+		background-repeat: no-repeat;
+		background-size: 100%;
+	}
+	.inputs-field {
+		height: 24px;
+		width: 150px;
+		border-radius: 4px;
+		border: 1px solid #333333;
+		padding: 0 10px;
+		margin: 2.5px 5px 2.5px 5px;
+	}
+	ul {
+		padding-left: 0px;
+		margin-right: 50px;
+	}
+	.confirm-btn {
+		position: absolute;
+		right: 40px;
+		bottom: 40px;
 		height: 30px;
-		width: 30px;
-		border-radius: 999px;
-		background: #ff4c3d;
+		width: 100px;
+		background: #42b983;
+		border-radius: 4px;
+		border: none;
 		color: #ffffff;
 	}
 

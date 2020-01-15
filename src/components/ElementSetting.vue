59c3bd5032b9656fd01006ccdcd7e727283b1766
Shuqi Liao <373628977@qq.com>
2020-01-15 16:27:27 +0800

    finish setting delete function

--- src/components/ElementSetting.vue
+++ src/components/ElementSetting.vue
@@ -2,9 +2,18 @@
 	<div class="setting-container">
 		<span class="title">{{type}} Setting</span>
 		<button class="close-btn" @click="_closeSetting"></button>
-		<ul>
-			<li v-for="(element, index) in elementList" :key="index">{{element}}</li>
-		</ul>
+		<div class="element-container">
+			<ul class="element-ul">
+				<li class="element-li"
+						:style="bgColor"
+						v-for="(element, index) in elementList"
+						:key="index"
+						@click="_selectItem(index, $event)">
+					{{element}}
+				</li>
+			</ul>
+		</div>
+		<button class="delete-btn" @click="_deleteItem">Delete</button>
 	</div>
 </template>
 
@@ -19,16 +28,44 @@
 			elementList: {
 				default: null,
 				type: Array
-			}
+			},
 		},
 		data() {
 			return {
-
+				typeColor: '#42B982',
+				currentSelectItem: null,
+				currentSelectIndex: -1,
+			}
+		},
+		computed: {
+			bgColor: function () {
+				return {
+					background: this.typeColor
+				}
 			}
 		},
 		methods: {
 			_closeSetting: function () {
 				this.$emit("closeElementSetting");
+			},
+			_selectItem: function (index, event) {
+				this.currentSelectIndex = index;
+				if (this.currentSelectItem != null) {
+					this.currentSelectItem.style.background = this.typeColor;
+				}
+				this.currentSelectItem = event.target;
+				this.currentSelectItem.style.background = "#1C86EE";
+			},
+			_deleteItem: function () {
+				if (this.currentSelectItem != null && this.currentSelectIndex != -1) {
+					this.currentSelectItem.style.background = this.typeColor;
+					this.elementList.splice(this.currentSelectIndex, 1);
+				}
+			}
+		},
+		mounted() {
+			if (!this.type.includes('PureFunction')) {
+				this.typeColor = '#34495D';
 			}
 		}
 	}
@@ -38,7 +75,7 @@
 	.setting-container {
 		position: relative;
 		width: 550px;
-		height: 350px;
+		height: 400px;
 		background: #ffffff;
 		border-radius: 10px;
 		display: flex;
@@ -64,6 +101,52 @@
 	}
 	.close-btn:hover {
 		background-image: url("../assets/close_btn_hover.png");
+	}
+	.element-container {
+		width: 80%;
+		height: 60%;
+		padding: 20px;
+		margin: 25px 0;
+		background-color: #ffffff;
+		box-shadow: 0px 0px 4px 0px #dcdcdc;
+		border-radius: 4px;
+	}
+	.element-ul {
+		width: 100%;
+		max-height: 100%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		padding: 0;
+		margin: 0;
+		overflow-y: scroll;
+	}
+	.element-li {
+		border-radius: 4px;
+		font-size: 12px;
+		font-weight: bold;
+		height: 36px;
+		color: #ffffff;
+		line-height: 36px;
+		padding: 0 10px;
+		margin: 5px;
+		border: 2px solid transparent;
+		cursor: pointer;
+	}
+	.element-li:hover {
+		border: 2px solid #1c86ee;
+	}
+	.delete-btn {
+		height: 30px;
+		width: 100px;
+		color: #ffffff;
+		font-weight: bold;
+		border-radius: 4px;
+		background: #315B96;
+		cursor: pointer;
+	}
+	.delete-btn:hover {
+		background: #EA5E5E;
 	}
 
 </style>
